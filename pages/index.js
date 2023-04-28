f66afd2ec3df721e3406f7740058834df0095b4d
charafimarouane <charafimarouane007@gmail.com>
2023-04-28 19:49:22 +0100

    adding Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Nav from "@/components/Nav"
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
@@ -16,7 +17,10 @@ export default function Home() {
   }  
 
   return(
-    <div>Logged in {session.user.email}</div>
+    <div className="bg-blue-900 min-h-screen">
+      <Nav/>
+      <div>Logged in {session.user.email}</div>
+    </div>
   )
 }
 
