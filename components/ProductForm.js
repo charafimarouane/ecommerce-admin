c4c9e3cf2b24533efb55736ac6189fcdd4b2be28
charafimarouane <charafimarouane007@gmail.com>
2023-06-08 04:46:53 +0200

    adding product images setting

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 import { useRouter } from "next/router"
 import { useState } from "react"
+import Spinner from './Spinner'
+import { ReactSortable } from "react-sortablejs"
 
 
 export default function ProductForm({
@@ -8,18 +10,20 @@ export default function ProductForm({
     title: existingTitle,
     description: existingDesctiption,
     price: existingPrice,
-    images,
+    images: existingImages,
     }){
 
         const [title, setTitle] = useState(existingTitle || '')
         const [description, setDescripton] = useState(existingDesctiption || '')
         const [price, setPrice] = useState(existingPrice || '')
+        const [images, setImages] = useState(existingImages || [])
         const [goToProducts, setGoToProducts] = useState(false)
+        const [isUploading, setIsUploading] = useState(false)
         const router = useRouter()
       
         async function saveProduct(ev){
             ev.preventDefault();
-            const data = {title, description, price}
+            const data = {title, description, price, images}
 
             if (_id) {
                 //update
@@ -38,14 +42,21 @@ export default function ProductForm({
         async function uploadImages(ev){
             const files = ev.target?.files
             if (files?.length > 0) {
+                setIsUploading(true)
                 const data = new FormData()
                 for(const file of files){
                     data.append('file', file)
                 }
-                
-                const res = await axios.post('/api/upload', data)
-                console.log(res.data);
+               const res = await axios.post('/api/upload', data)    
+                setImages(oldImages => {
+                    return [...oldImages, ...res.data.links]
+                })
+                setIsUploading(false)
             }
+        }
+
+        function updateImagesOrder(images){
+            setImages(images)
         }
         
         return(
@@ -61,7 +72,22 @@ export default function ProductForm({
                     <label>
                         Photos
                     </label>
-                    <div className="mb-2">
+                    <div className="mb-2 flex flex-wrap gap-2">
+                    <ReactSortable 
+                      list={images} 
+                      setList={updateImagesOrder}
+                      className="flex flex-wrap gap-1">                     
+                        {!!images?.length && images.map(link => (
+                            <div key={link} className="h-24 ">
+                                <img src={link} alt="" className="rounded-lg"/>
+                            </div>
+                        ))}
+                    </ReactSortable>
+                        {isUploading && (
+                            <div className="h-24 flex items-center bg-gray-300 rounded-lg p-4">
+                                <Spinner/>
+                            </div>
+                        )}
                         <label className="w-24 h-24 cursor-pointer flex justify-center items-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-300"> 
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
@@ -69,9 +95,6 @@ export default function ProductForm({
                         Upload
                         <input type="file" className="hidden" onChange={uploadImages}/>
                         </label>
-                        {!images?.length && (
-                            <div>No photos in this product</div>
-                        )}
                     </div>
                     <label>Desctiption</label>
                     <textarea 
