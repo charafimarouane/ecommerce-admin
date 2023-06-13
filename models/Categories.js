f85f67f10811f5114b219705fc43fb34f5b8cdc9
charafimarouane <charafimarouane007@gmail.com>
2023-06-13 05:50:49 +0200

    adding properties to db

--- models/Categories.js
+++ models/Categories.js
@@ -3,6 +3,7 @@ const { Schema, models, model, default: mongoose } = require("mongoose");
 const CategoriesSchema = new Schema({
     name: {type: String, required: true},
     parent:{type: mongoose.Types.ObjectId, ref:'Category'},
+    properties: [{type : Object}]
 });
 
 export const Category = models?.Category || model('Category', CategoriesSchema)
