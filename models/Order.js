5af471d6cdccbc92d8eaaf274932cba2d5efbcc2
charafimarouane <charafimarouane007@gmail.com>
2023-06-22 20:05:38 +0200

    completing order section

---
+++ models/Order.js
@@ -0,0 +1,15 @@
+const { Schema, models, model } = require("mongoose");
+
+const OrderSchema = new Schema({
+    line_items: Object,
+    name: String,
+    email: String,
+    city: String,
+    postalcode: String,
+    adress: String,
+    paid: Boolean,
+},{
+    timestamps:true,
+})
+
+export const Order = models?.Order || model('Order', OrderSchema)
